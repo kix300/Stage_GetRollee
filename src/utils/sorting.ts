625e1d48c6e2a1106b4c384465a497e88df01985
kix300 <kixwalkiki@gmail.com>
2026-01-15 15:47:04 +0100

    Remove Todo comment

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,93 +1,93 @@
 export type SortOrder = 'asc' | 'desc';
 
 export interface SortConfig<T> {
-  column: keyof T;
-  order: SortOrder;
+	column: keyof T;
+	order: SortOrder;
 }
 
 export function stableSort<T extends Record<string, any>>(
-  data: T[],
-  column: keyof T,
-  order: SortOrder = 'asc',
-  secondaryColumn: keyof T = 'name'
+	data: T[],
+	column: keyof T,
+	order: SortOrder = 'asc',
+	secondaryColumn: keyof T = 'name'
 ): T[] {
-  const indexed = data.map((item, index) => ({ item, index }));
+	const indexed = data.map((item, index) => ({ item, index }));
 
-  indexed.sort((a, b) => {
-    const valueA = a.item[column];
-    const valueB = b.item[column];
+	indexed.sort((a, b) => {
+		const valueA = a.item[column];
+		const valueB = b.item[column];
 
-    let compA = valueA;
-    let compB = valueB;
+		let compA = valueA;
+		let compB = valueB;
 
-    if (typeof valueA === 'string') {
-      compA = valueA.toLowerCase();
-    }
-    if (typeof valueB === 'string') {
-      compB = valueB.toLowerCase();
-    }
+		if (typeof valueA === 'string') {
+			compA = valueA.toLowerCase();
+		}
+		if (typeof valueB === 'string') {
+			compB = valueB.toLowerCase();
+		}
 
-    let comparison = 0;
-    if (compA < compB) {
-      comparison = -1;
-    } else if (compA > compB) {
-      comparison = 1;
-    } else {
-      if (column !== secondaryColumn) {
-        const secA = a.item[secondaryColumn];
-        const secB = b.item[secondaryColumn];
-        
-        let secCompA = secA;
-        let secCompB = secB;
-        
-        if (typeof secA === 'string') {
-          secCompA = secA.toLowerCase();
-        }
-        if (typeof secB === 'string') {
-          secCompB = secB.toLowerCase();
-        }
-        
-        if (secCompA < secCompB) {
-          comparison = -1;
-        } else if (secCompA > secCompB) {
-          comparison = 1;
-        } else {
-          comparison = a.index - b.index;
-        }
-      } else {
-        comparison = a.index - b.index;
-      }
-    }
+		let comparison = 0;
+		if (compA < compB) {
+			comparison = -1;
+		} else if (compA > compB) {
+			comparison = 1;
+		} else {
+			if (column !== secondaryColumn) {
+				const secA = a.item[secondaryColumn];
+				const secB = b.item[secondaryColumn];
 
-    if (order === 'desc' && comparison !== 0) {
-      const primaryA = a.item[column];
-      const primaryB = b.item[column];
-      
-      let primCompA = primaryA;
-      let primCompB = primaryB;
-      
-      if (typeof primaryA === 'string') primCompA = primaryA.toLowerCase();
-      if (typeof primaryB === 'string') primCompB = primaryB.toLowerCase();
-      
-      if (primCompA !== primCompB) {
-        return -comparison;
-      }
-    }
+				let secCompA = secA;
+				let secCompB = secB;
 
-    return comparison;
-  });
+				if (typeof secA === 'string') {
+					secCompA = secA.toLowerCase();
+				}
+				if (typeof secB === 'string') {
+					secCompB = secB.toLowerCase();
+				}
 
-  return indexed.map(({ item }) => item);
+				if (secCompA < secCompB) {
+					comparison = -1;
+				} else if (secCompA > secCompB) {
+					comparison = 1;
+				} else {
+					comparison = a.index - b.index;
+				}
+			} else {
+				comparison = a.index - b.index;
+			}
+		}
+
+		if (order === 'desc' && comparison !== 0) {
+			const primaryA = a.item[column];
+			const primaryB = b.item[column];
+
+			let primCompA = primaryA;
+			let primCompB = primaryB;
+
+			if (typeof primaryA === 'string') primCompA = primaryA.toLowerCase();
+			if (typeof primaryB === 'string') primCompB = primaryB.toLowerCase();
+
+			if (primCompA !== primCompB) {
+				return -comparison;
+			}
+		}
+
+		return comparison;
+	});
+
+	return indexed.map(({ item }) => item);
 }
 
 export function toggleSortOrder(
-  currentColumn: string,
-  newColumn: string,
-  currentOrder: SortOrder
+	currentColumn: string,
+	newColumn: string,
+	currentOrder: SortOrder
 ): SortOrder {
-  if (currentColumn === newColumn) {
-    return currentOrder === 'asc' ? 'desc' : 'asc';
-  }
-  return 'asc';
+	if (currentColumn === newColumn) {
+		return currentOrder === 'asc' ? 'desc' : 'asc';
+	}
+	return 'asc';
 }
 
